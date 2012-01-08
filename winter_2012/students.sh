0f0c3509c66fde0afce1e2027f0047cd6477c17b
Jon Jacky <jon@u.washington.edu>
2012-01-08 12:38:11 -0800

    add more enties to students.sh, add link to students.html

---
+++ winter_2012/students.sh
@@ -0,0 +1,30 @@
+# Set environment variables, short names for students' servers
+# Variables are the same as github username with '-' removed, all lowercase
+# To execute this script in the bash shell: source students.sh
+# After than, can login by (for example) ssh class@$uwstudent
+#  and can contact echo server by python echo_client.py $uwstudent
+
+export uwstudent="block335026-fa5.blueboxgrid.com"  # uw-student (sample)
+
+export bbloss="block335027-tng.blueboxgrid.com"  # Ben B.
+export scboyd="block335037-mrw.blueboxgrid.com"  # Scott B.
+export cushride="block335051-khm.blueboxgrid.com"  # Chris K.
+export glamb="block335044-q6t.blueboxgrid.com"  # Gregg L.
+export jennyliang="block335052-4ym.blueboxgrid.com"  # Jenny L.
+
+export diamar="block335048-dnh.blueboxgrid.com"  # Diana M.
+export dalanmiller="block335042-abq.blueboxgrid.com"  # Daniel M.
+export mincet="block335046-zme.blueboxgrid.com"  # Tony M.
+export mukhtyar="block335030-p73.blueboxgrid.com"  # Shruti M.
+export abdulabulbulamir="block335049-vpv.blueboxgrid.com"  # Andy N.
+
+export uwshashi="block335043-vhh.blueboxgrid.com"  # Shashi P.
+export grivera="block335054-unf.blueboxgrid.com"  # Guillermo R.
+export ctryan3="block335032-t4f.blueboxgrid.com"  # Conor R.
+export shifow="block335055-hnc.blueboxgrid.com"  # Kamal S.
+export dodecatheon="block335041-sbd.blueboxgrid.com"  # Ted S.
+
+export polymerjazz="block335034-6uy.blueboxgrid.com"  # Dimitry V.
+export alexuw="block335029-tim.blueboxgrid.com"  # Alex W.
+export joshwest1="block335035-8sl.blueboxgrid.com"  # Josh W.
+export mwickham="block335045-pck.blueboxgrid.com"  # Michelle W.
