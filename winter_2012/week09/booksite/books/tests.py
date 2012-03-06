ec49d4e6b8069f16ee8dc635b73fd3bbcc0292ca
Jon Jacky <jon@uw.edu>
2012-03-06 14:19:03 -0800

    finish lecture notes for week 9

--- winter_2012/week09/booksite/books/tests.py
+++ winter_2012/week09/booksite/books/tests.py
@@ -11,10 +11,10 @@ class BooksTest(TestCase):
     def test_empty(self):
         # there must be a templates/404.html for this test to pass
         response = self.client.get('/')
-        print response.status_code
+        #print response.status_code
         self.assertEqual(response.status_code, 404) # no page
 
     def test_books(self):
         response = self.client.get('/books/')
-        print response.status_code
+        #print response.status_code
         self.assertEqual(response.status_code, 200) # index page
